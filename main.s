3bfb15d4d4e2ee7f76f7577a2ba52e050d3ddbc5
ktk219 <ktk219@skbl-422-002.ph.ic.ac.uk>
2021-11-23 12:03:36 +0000

    started project

--- main.s
+++ main.s
@@ -9,70 +9,27 @@ psect	udata_acs   ; reserve data space in access ram
 counter:    ds 1    ; reserve one byte for a counter variable
 delay_count:ds 1    ; reserve one byte for counter in the delay routine
     
-psect	udata_bank4 ; reserve data anywhere in RAM (here at 0x400)
-myArray:    ds 0x80 ; reserve 128 bytes for message data
-
-psect	data    
-	; ******* myTable, data in programme memory, and its length *****
-myTable:
-	db	' ','e','l','l','o',' ','W','o','r','l','d','!',0x0a
-					; message, plus carriage return
-	myTable_l   EQU	13	; length of data
-	align	2
+org 0x00
+ 
+setup:
+    movlw 0xff
+    movwf TRISB
+    movlw 0xff
+    movwf PORTB
+    call Bigdelay
+    movlw   0x00
+    call Bigdelay
+    goto setup
     
-psect	code, abs	
-rst: 	org 0x0
- 	goto	setup
-
-	; ******* Programme FLASH read Setup Code ***********************
-setup:	bcf	CFGS	; point to Flash program memory  
-	bsf	EEPGD 	; access Flash program memory
-	call	UART_Setup	; setup UART
-	call	LCD_Setup	; setup UART
-	call	ADC_Setup	; setup ADC
-	goto	start
-	
-	; ******* Main programme ****************************************
-start: 	lfsr	0, myArray	; Load FSR0 with address in RAM	
-	movlw	low highword(myTable)	; address of data in PM
-	movwf	TBLPTRU, A		; load upper bits to TBLPTRU
-	movlw	high(myTable)	; address of data in PM
-	movwf	TBLPTRH, A		; load high byte to TBLPTRH
-	movlw	low(myTable)	; address of data in PM
-	movwf	TBLPTRL, A		; load low byte to TBLPTRL
-	movlw	myTable_l	; bytes to read
-	movwf 	counter, A		; our counter register
-	
-	;call	multiply_24
-
-loop: 	tblrd*+			; one byte from PM to TABLAT, increment TBLPRT
-	movff	TABLAT, POSTINC0; move data from TABLAT to (FSR0), inc FSR0	
-	decfsz	counter, A		; count down to zero
-	bra	loop		; keep going until finished
-		
-	movlw	myTable_l	; output message to UART
-	lfsr	2, myArray
-	call	UART_Transmit_Message
-
-	movlw	myTable_l-1	; output message to LCD
-				; don't send the final carriage return to LCD
-	lfsr	2, myArray
-	call	LCD_Write_Message
-	
-measure_loop:
-	call	ADC_Read
-	;movf	ADRESH, W, A
-	call	decimal
-	movf	0x00, W, A
-	call	LCD_Write_Hex
-	movf	0x01, W, A
-	call	LCD_Write_Hex
-
-	goto	measure_loop		; goto current line in code
-	
-	; a delay subroutine if you need one, times around loop in delay_count
-delay:	decfsz	delay_count, A	; decrement until zero
-	bra	delay
-	return
-
-	end	rst
+   
+   
+Bigdelay: 
+	movlw high(0xDEAD)
+	movwf 0x10,A
+	movlw low(0xDEAD)
+	movwf 0x11,A
+	movlw 0x00 ; W = 0
+Dloop:	decf 0x11, f, A ; counter decrement
+	subwfb 0x10, f, A
+	bc Dloop
+	return   
