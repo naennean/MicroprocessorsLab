811119921e8b35be2bf305015c1d0c6ab50f2f5a
Melanie <97545983+naennean@users.noreply.github.com>
2022-02-14 10:47:27 +0000

    Writes incrementing numbers to block of RAM using FSR0

--- main.s
+++ main.s
@@ -7,18 +7,23 @@ main:
 	goto	start
 
 	org	0x100		    ; Main code starts here at address 0x100
+
 start:
+	myLoc EQU 0x2E0
+	lfsr    0,myLoc
 	movlw 	0x0
-	movwf	TRISC, A	    ; Port C all outputs
+;	movwf	TRISC, A	    ; Port C all outputs
 	bra 	test
-loop:
-	movff 	0x06, PORTC
+inc_numb:
+;	movff 	0x06, PORTC
 	incf 	0x06, W, A
 test:
 	movwf	0x06, A	    ; Test for end of loop condition
+	movff	0x06, POSTINC0 ;store new number into predetermined loc
 	movlw 	0x63
 	cpfsgt 	0x06, A
-	bra 	loop		    ; Not yet finished goto start of loop again
-	goto 	0x0		    ; Re-run program from start
+	bra 	inc_numb	    ; Not yet finished goto start of loop again
+	goto 	$ ;0x0		    ; Re-run program from start
+
 
 	end	main
