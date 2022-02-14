2866875309633a6d74e37c121e2b611f701c3c08
Melanie <97545983+naennean@users.noreply.github.com>
2022-02-14 11:23:59 +0000

    Uses FSR1 to read data

--- main.s
+++ main.s
@@ -20,10 +20,25 @@ inc_numb:
 test:
 	movwf	0x06, A	    ; Test for end of loop condition
 	movff	0x06, POSTINC0 ;store new number into predetermined loc
-	movlw 	0x63
+	movlw 	0x05
 	cpfsgt 	0x06, A
 	bra 	inc_numb	    ; Not yet finished goto start of loop again
-	goto 	$ ;0x0		    ; Re-run program from start
-
-
+	;goto 	$ ;0x0		    ; Re-run program from start
+start2:
+    	lfsr	1,myLoc
+	movlw 	0x0
+	bra 	read
+inc_numb2:
+	incf 	0x06, W, A
+read:
+	movwf	0x06, A
+	movff	POSTINC1,0x05
+	cpfseq	0x05,A
+	bra	light
+	movlw	0x05
+	cpfsgt	0x06, A
+	bra	inc_numb2
+	
+light:
+	NOP
 	end	main
