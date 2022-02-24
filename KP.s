a178aff49f5b6448111a02e8fc889a3c0a5d54db
mf919 <mf919@skbl-422-005.ph.ic.ac.uk>
2022-02-24 11:58:26 +0000

    Got unique code for each button pressed on keyboard

--- KP.s
+++ KP.s
@@ -1,7 +1,7 @@
 
     #include <xc.inc>
 
-global  KP_Setup, KP_read_row, KP_read_col
+global  KP_Setup, KP_read_row, KP_read_col,read_row, read_col
 
 psect	udata_acs   ; named variables in access ram
 read_row:	ds 1	; reserve one byte for row values
@@ -14,93 +14,42 @@ LCD_counter:	ds 1   ; reserve 1 byte for counting through nessage
 
 	LCD_E	EQU 5	; LCD enable bit
     	LCD_RS	EQU 4	; LCD register select bit
+	
 
-psect	lcd_code,class=CODE
+psect	data    
+	; ******* myTable, data in programme memory, and its length *****
+KP_numbers:
+	db	'H','e','l','l','o',' ','W','o','r','l','d','!',0x0a
+					; message, plus carriage return
+	myTable_l   EQU	13	; length of data
+	align	2
+
+psect	keypad_code,class=CODE
     
 KP_Setup:
 	movlb	0xf		; bank 15
 	bsf	PADCFG1, 6, B	 ;set REPU bit in PADCFG1 to enable pull-up resistors on PORTE
-	clrf	LATE		; write 0s to LATE register
+	clrf	LATE, A		; write 0s to LATE register
 	movlw	0x0f		; 00001111
 	movwf	TRISE, A	;configures PORTE 4-7 as outputs and PORTE 0-3 as inputs
 	return
 	
-KP_read_row:
+KP_read_col:	    ;Already switched row and col (Mel 24.02.22)
 	movlw	0x0f		; 00001111
 	movwf	TRISE, A	;configures PORTE 4-7 as outputs and PORTE 0-3 as inputs
 ;	movlw	0x0f
-	movff	PORTE, read_row, A
-	cpfslt	read_row, A
-	;add smth that shows whether button has been pressed
-	
+	call	LCD_delay
+	movff	PORTE, read_col , A  
 	return
-KP_read_col:
+	
+KP_read_row:
 	movlw	0xf0		; 11110000
-	;REMEMBER TO USE LW HIGHWORD OR SMTH
 	movwf	TRISE, A	;configures PORTE 4-7 as inputs and PORTE 0-3 as outputs
+	call	LCD_delay
+	movff	PORTE, read_row , A ;
 	
+	return
 	
-	
-
-LCD_Write_Message:	    ; Message stored at FSR2, length stored in W
-	movwf   LCD_counter, A
-LCD_Loop_message:
-	movf    POSTINC2, W, A
-	call    LCD_Send_Byte_D
-	decfsz  LCD_counter, A
-	bra	LCD_Loop_message
-	return
-
-LCD_Send_Byte_I:	    ; Transmits byte stored in W to instruction reg
-	movwf   LCD_tmp, A
-	swapf   LCD_tmp, W, A   ; swap nibbles, high nibble goes first
-	andlw   0x0f	    ; select just low nibble
-	movwf   LATB, A	    ; output data bits to LCD
-	bcf	LATB, LCD_RS, A	; Instruction write clear RS bit
-	call    LCD_Enable  ; Pulse enable Bit 
-	movf	LCD_tmp, W, A   ; swap nibbles, now do low nibble
-	andlw   0x0f	    ; select just low nibble
-	movwf   LATB, A	    ; output data bits to LCD
-	bcf	LATB, LCD_RS, A	; Instruction write clear RS bit
-        call    LCD_Enable  ; Pulse enable Bit 
-	return
-
-LCD_Send_Byte_D:	    ; Transmits byte stored in W to data reg
-	movwf   LCD_tmp, A
-	swapf   LCD_tmp, W, A	; swap nibbles, high nibble goes first
-	andlw   0x0f	    ; select just low nibble
-	movwf   LATB, A	    ; output data bits to LCD
-	bsf	LATB, LCD_RS, A	; Data write set RS bit
-	call    LCD_Enable  ; Pulse enable Bit 
-	movf	LCD_tmp, W, A	; swap nibbles, now do low nibble
-	andlw   0x0f	    ; select just low nibble
-	movwf   LATB, A	    ; output data bits to LCD
-	bsf	LATB, LCD_RS, A	; Data write set RS bit	    
-        call    LCD_Enable  ; Pulse enable Bit 
-	movlw	10	    ; delay 40us
-	call	LCD_delay_x4us
-	return
-
-LCD_Enable:	    ; pulse enable bit LCD_E for 500ns
-	nop
-	nop
-	nop
-	nop
-	nop
-	nop
-	nop
-	nop
-	bsf	LATB, LCD_E, A	    ; Take enable high
-	nop
-	nop
-	nop
-	nop
-	nop
-	nop
-	nop
-	bcf	LATB, LCD_E, A	    ; Writes data to LCD
-	return
-    
 ; ** a few delay routines below here as LCD timing can be quite critical ****
 LCD_delay_ms:		    ; delay given in ms in W
 	movwf	LCD_cnt_ms, A
@@ -129,6 +78,19 @@ lcdlp1:	decf 	LCD_cnt_l, F, A	; no carry when 0x00 -> 0xff
 	return			; carry reset so return
 
 
+	
+KP_decode:
+
+    ;;;;; ADD 17 FUnctions to decode keypad and send ASCII to LCD display
+    
+    
+;	movlw	0x01 ; (binary for button 1~)
+;	cpfseq PORTD, A
+;	call print 1 to LCD
+;	movlw binary for button 2
+;	cpfseq 2
+;	call print 2
+	...
     end
 
 
--- main.s
+++ main.s
@@ -1,63 +1,59 @@
-#include <xc.inc>
+	#include <xc.inc>
+extrn	KP_Setup, KP_read_col,KP_read_row,read_row, read_col
 
-extrn	UART_Setup, UART_Transmit_Message  ; external subroutines
-extrn	LCD_Setup, LCD_Write_Message
-	
 psect	udata_acs   ; reserve data space in access ram
-counter:    ds 1    ; reserve one byte for a counter variable
 delay_count:ds 1    ; reserve one byte for counter in the delay routine
+delaydelay_count:ds 1
+delayCubed_count:ds 1	
     
-psect	udata_bank4 ; reserve data anywhere in RAM (here at 0x400)
-myArray:    ds 0x80 ; reserve 128 bytes for message data
-
-psect	data    
-	; ******* myTable, data in programme memory, and its length *****
-myTable:
-	db	'H','e','l','l','o',' ','W','o','r','l','d','!',0x0a
-					; message, plus carriage return
-	myTable_l   EQU	13	; length of data
-	align	2
     
-psect	code, abs	
-rst: 	org 0x0
- 	goto	setup
-
-	; ******* Programme FLASH read Setup Code ***********************
-setup:	bcf	CFGS	; point to Flash program memory  
-	bsf	EEPGD 	; access Flash program memory
-	call	UART_Setup	; setup UART
-	call	LCD_Setup	; setup UART
-	goto	start
+psect	code, abs
 	
-	; ******* Main programme ****************************************
-start: 	lfsr	0, myArray	; Load FSR0 with address in RAM	
-	movlw	low highword(myTable)	; address of data in PM
-	movwf	TBLPTRU, A		; load upper bits to TBLPTRU
-	movlw	high(myTable)	; address of data in PM
-	movwf	TBLPTRH, A		; load high byte to TBLPTRH
-	movlw	low(myTable)	; address of data in PM
-	movwf	TBLPTRL, A		; load low byte to TBLPTRL
-	movlw	myTable_l	; bytes to read
-	movwf 	counter, A		; our counter register
-loop: 	tblrd*+			; one byte from PM to TABLAT, increment TBLPRT
-	movff	TABLAT, POSTINC0; move data from TABLAT to (FSR0), inc FSR0	
-	decfsz	counter, A		; count down to zero
-	bra	loop		; keep going until finished
-		
-	movlw	myTable_l	; output message to UART
-	lfsr	2, myArray
-	call	UART_Transmit_Message
+main:
+	org	0x0
+	goto	setup
 
-	movlw	myTable_l	; output message to LCD
-	addlw	0xff		; don't send the final carriage return to LCD
-	lfsr	2, myArray
-	call	LCD_Write_Message
-
-	goto	$		; goto current line in code
-
-	; a delay subroutine if you need one, times around loop in delay_count
-delay:	decfsz	delay_count, A	; decrement until zero
-	bra	delay
+	org	0x100		    ; Main code starts here at address 0x100
+setup:
+	movlw	0x00		; 
+	movwf	TRISD, A	;configures PORTD as output
+	goto    start
+start:
+	call	KP_Setup
+read:
+	call	KP_read_col
+	call	KP_read_row
+	movff	read_row, WREG
+	addwf	read_col, W, A
+;	movwf	PORTD, A
+;	movff	read_col, WREG
+	movwf	PORTD, A
+	goto 	read
+	
+	
+delay:
+	movlw	0xff
+	movwf	delay_count	    ;store 0xff in 0x02 for delay
+delayloop:
+	movlw	0xff
+	movwf	delaydelay_count	    ;store 0xff in 0x02 for delay	
+	call	delaydelay
+	decfsz  delay_count		;decrement from 0x20 down to 0
+	bra	delayloop		;when line above reaches zero, will skip this line
 	return
-
-	end	rst
+	
+delaydelay:
+	movlw	0xff
+	movwf	delayCubed_count	    ;store 0xff in 0x02 for delay	
+	call	delayCubed
+	decfsz	delaydelay_count
+	bra	delaydelay
+	return
+delayCubed:
+	decfsz	delayCubed_count
+	bra	delayCubed
+	return
+	
+	
+	
+	end	main
